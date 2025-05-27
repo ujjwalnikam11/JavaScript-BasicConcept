798e41e21a20c31bb053c4acd3264b4374551ec9
ujjwalnikam11 <ujjwalnikam11971@gmail.com>
2025-05-27 06:32:37 +0000

    changes

---
+++ .github/01_Basic/array.js
@@ -0,0 +1,8 @@
+let numbers = ['Blue', 2, 3, 4, 5]; // array
+numbers[2] = 10; // array
+
+numbers[3] = undefined; // array
+numbers[4] = null; // array
+console.log(numbers); // array
+console.log(numbers.length); // array
+
---
+++ .github/01_Basic/fun.js
@@ -0,0 +1,13 @@
+function greet(name, age) {
+    console.log('Hello ' + name + ' ' + age);
+}
+
+greet('John', 20); // Hello John 20
+greet('Jane', 25); // Hello Jane 25
+
+// Calculate function
+function calculate(num1, num2) {
+    return num1 + num2;
+}
+
+console.log(calculate(2, 3)); // 5
