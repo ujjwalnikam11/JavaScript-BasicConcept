5c4abffcc1456fd4f237c9271a6f1fa03945b012
ujjwalnikam11 <ujjwalnikam11971@gmail.com>
2025-05-28 07:38:27 +0000

    operator

--- .github/01_Basic/operators.js
+++ .github/01_Basic/operators.js
@@ -1,6 +1,26 @@
-let text1 = "A";
+/*let text1 = "A";
 let text2 = "B";
 let result = text1 < text2;
-console.log(result);
-text1
+console.log(result);text1*/
 
+/*let text1 = "20";
+let text2 = "5";
+let result = text1 < text2;
+console.log(result);/*
+
+/*let text1 = "John";
+let text2 = "Doe";
+let text3 = text1 + " " + text2;
+console.log(text3);*/ 
+
+/*let text1 = "What a very";
+let text2 = "nice day";
+let text3 = text1 + " " + text2;
+console.log(text3);*/
+
+/*let x = 5 + 5;
+let y = "5" + 5;
+let z = "Hello" + 5;
+console.log(x); 
+console.log(y); 
+console.log(z);*/ 
